21e008b1ddef2be831b38a3a58c63ac607d99b54
Németh Tamás <tamas.nemeth2002@gmail.com>
2026-09-05 16:28:11 +0200

    Initial Commit

---
+++ target/debug/deps/bareeye-785664bb793e5811.d
@@ -0,0 +1,5 @@
+C:\Users\User\Desktop\randomprojectsillneverfinish\BareEye\target\debug\deps\bareeye-785664bb793e5811.d: src\main.rs
+
+C:\Users\User\Desktop\randomprojectsillneverfinish\BareEye\target\debug\deps\libbareeye-785664bb793e5811.rmeta: src\main.rs
+
+src\main.rs:
---
+++ target/debug/deps/bareeye-8d7706c1373658ee.d
@@ -0,0 +1,5 @@
+C:\Users\User\Desktop\randomprojectsillneverfinish\BareEye\target\debug\deps\bareeye-8d7706c1373658ee.d: src\main.rs
+
+C:\Users\User\Desktop\randomprojectsillneverfinish\BareEye\target\debug\deps\libbareeye-8d7706c1373658ee.rmeta: src\main.rs
+
+src\main.rs:
